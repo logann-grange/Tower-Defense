ba7276b0721a710f304c697fc8a8272e5d448bc5
Daroueche MARI <daroueche.mari@laplateforme.io>
2026-06-04 11:20:20 +0200

    Test Map+Tower

---
+++ build/CMakeFiles/TowerDefense.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/dmari/Bureau/Cours JVSI/Start C++/Tower-Defense-Mohamed/src/Map.cpp" "CMakeFiles/TowerDefense.dir/src/Map.cpp.obj" "gcc" "CMakeFiles/TowerDefense.dir/src/Map.cpp.obj.d"
+  "C:/Users/dmari/Bureau/Cours JVSI/Start C++/Tower-Defense-Mohamed/src/Projectile.cpp" "CMakeFiles/TowerDefense.dir/src/Projectile.cpp.obj" "gcc" "CMakeFiles/TowerDefense.dir/src/Projectile.cpp.obj.d"
+  "C:/Users/dmari/Bureau/Cours JVSI/Start C++/Tower-Defense-Mohamed/src/Tour.cpp" "CMakeFiles/TowerDefense.dir/src/Tour.cpp.obj" "gcc" "CMakeFiles/TowerDefense.dir/src/Tour.cpp.obj.d"
+  "C:/Users/dmari/Bureau/Cours JVSI/Start C++/Tower-Defense-Mohamed/src/main.cpp" "CMakeFiles/TowerDefense.dir/src/main.cpp.obj" "gcc" "CMakeFiles/TowerDefense.dir/src/main.cpp.obj.d"
+  "C:/Users/dmari/Bureau/Cours JVSI/Start C++/Tower-Defense-Mohamed/src/mob/Monster.cpp" "CMakeFiles/TowerDefense.dir/src/mob/Monster.cpp.obj" "gcc" "CMakeFiles/TowerDefense.dir/src/mob/Monster.cpp.obj.d"
+  "C:/Users/dmari/Bureau/Cours JVSI/Start C++/Tower-Defense-Mohamed/src/mob/Skeleton.cpp" "CMakeFiles/TowerDefense.dir/src/mob/Skeleton.cpp.obj" "gcc" "CMakeFiles/TowerDefense.dir/src/mob/Skeleton.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/TowerDefense.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TowerDefense.dir/src/Map.cpp.obj"
+  "CMakeFiles/TowerDefense.dir/src/Map.cpp.obj.d"
+  "CMakeFiles/TowerDefense.dir/src/Projectile.cpp.obj"
+  "CMakeFiles/TowerDefense.dir/src/Projectile.cpp.obj.d"
+  "CMakeFiles/TowerDefense.dir/src/Tour.cpp.obj"
+  "CMakeFiles/TowerDefense.dir/src/Tour.cpp.obj.d"
+  "CMakeFiles/TowerDefense.dir/src/main.cpp.obj"
+  "CMakeFiles/TowerDefense.dir/src/main.cpp.obj.d"
+  "CMakeFiles/TowerDefense.dir/src/mob/Monster.cpp.obj"
+  "CMakeFiles/TowerDefense.dir/src/mob/Monster.cpp.obj.d"
+  "CMakeFiles/TowerDefense.dir/src/mob/Skeleton.cpp.obj"
+  "CMakeFiles/TowerDefense.dir/src/mob/Skeleton.cpp.obj.d"
+  "TowerDefense.exe"
+  "TowerDefense.exe.manifest"
+  "TowerDefense.pdb"
+  "libTowerDefense.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TowerDefense.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
